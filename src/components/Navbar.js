8c214716599807debd03e5016946422cd50370e7
MD5HashBrowns <md5hashbrownz@gmail.com>
2018-03-06 21:06:35 -0500

    Made mobile search faster (by removing previews

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Input from 'material-ui/Input'
 import { withRouter, Link } from 'react-router-dom'
+import { isBrowser } from 'react-device-detect'
 
 class Navbar extends Component {
 	constructor() {
@@ -16,12 +17,16 @@ class Navbar extends Component {
 		this.props.history.push('/search/' + this.state.searchTerm)
 	}
 	handleChange(e) {
-		this.setState({searchTerm: e.target.value}, this.updateHistory)
+		// Only do live updates on desktop browser, where on mobile (or anything else) you have to hit your (virtual) enter key
+		if (isBrowser) {
+			this.setState({ searchTerm: e.target.value }, this.updateHistory)
+		} else {
+			this.setState({ searchTerm: e.target.value })
+		}
 	}
 	handleSubmit(e) {
 		e.preventDefault()
 		this.updateHistory()
-
 	}
 	render() {
 		return (
